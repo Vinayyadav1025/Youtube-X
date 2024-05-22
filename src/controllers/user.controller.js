90bdd36522935bdadb21a14d323bf95b592391de
vinayyadav <vinayyadavfzd29@gmail.com>
2024-05-22 15:34:59 +0530

    fixed some typoes mistakes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -118,7 +118,7 @@ const loginUser = asyncHandler(async (req,res) => {
 
     const {email, username, password} = req.body;
 
-    if(!username || !email){
+    if(!username && !email){
         throw new ApiError(400, "Username or email is required");
     }
 
--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,5 +1,5 @@
-import { ApiError } from "../utils/ApiError";
-import { asyncHandler } from "../utils/asyncHandler";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "json-web-token";
 import { User } from "../models/user.model.js";; 
 export const verifyJWT = asyncHandler(async (req,res, next) => {
